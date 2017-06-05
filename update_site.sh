c222834584376263fbe45030ed881ecd13115c5c
Chris McComb <ccmcc2012@gmail.com>
2017-06-05 10:48:51 -0400

    2017-06-05:10:48:49

--- update_site.sh
+++ update_site.sh
@@ -1,11 +1,29 @@
 # Build the site
-jekyll
+jekyll build
 
 # Clean the directory
-
+echo "Cleaning directory..."
+cd ../cmccomb.github.io
+git rm -rf ./*
+git clean -fxd
+cd ..
 
 # Copy over the completed website
-mv _site/* ../cmccomb.github.io
+echo "Copying static files"
+mv ~/GitHub/Websites/cmccomb.github.io-source/_site/* ~/GitHub/Websites/cmccomb.github.io/
 
 # Touch nojekyll
-touch ../cmccomb.github.io/.nojekyll
+touch ~/Github/Websites/cmccomb.github.io/.nojekyll
+
+# Add commit and push for deployment
+current_date_time="`date +%Y-%m-%d:%H:%M:%S`";
+cd cmccomb.github.io
+git add ./*
+git commit -a -m $current_date_time
+git push
+
+# Add commit and push for source
+cd ../cmccomb.github.io-source
+git add ./*
+git commit -a -m $current_date_time
+git push
