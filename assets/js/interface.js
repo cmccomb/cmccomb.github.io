7eb0ea85e8f4755af769746e0b7d8f909518cd54
Chris McComb <ccmcc2012@gmail.com>
2024-05-31 11:20:57 -0400

    Update interface.js

--- assets/js/interface.js
+++ assets/js/interface.js
@@ -1,13 +1,12 @@
 // button stuff
 document.getElementById("exit").onclick = function () {
     if (document.getElementById("exit").style.visibility == "visible") {
-    document.getElementById("profile").remove();
-    document.getElementById("footer").remove();
-    document.getElementById("graph-container").classList.remove("blur");
-    // document.getElementById("graph-container").style.visibility = "visible";
-    document.getElementById("graph-container").style.pointerEvents = "auto";
+        document.getElementById("profile").remove();
+        document.getElementById("footer").remove();
+        document.getElementById("graph-container").classList.remove("blur");
+        document.getElementById("graph-container").style.pointerEvents = "auto";
     } else {
-        window.open("https://ccm-search-my-publications.hf.space/");
+        window.open("https://ccm-search-my-publications.hf.space/", "_self");
     }
 }
 
