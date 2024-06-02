bf0211ab5de6c389657fdbc26a913d280066f615
Chris McComb <ccmcc2012@gmail.com>
2024-06-02 14:32:19 -0400

    Cleaning up

--- _scripts/build_graph.py
+++ _scripts/build_graph.py
@@ -7,54 +7,57 @@ import numpy
 import datasets
 import bs4
 
+# Geenral settings
+N = 10  # number of top citations to show
+padding = 1.2  # Padding to give around teh annotation
+
+# Load data from huggingface
 citations = datasets.load_dataset("ccm/publications")["train"].to_pandas()
 
+# Make a t-SNE embedding of the embeddings
 tsne_embeddings = sklearn.manifold.TSNE(n_components=2, random_state=42).fit_transform(
     numpy.stack(citations["embedding"].values)
 )
-tsne_embeddings = sklearn.decomposition.PCA(
+
+# Next do a PCA in order to figure out how best to orient the plot
+oriented_tsne_embeddings = sklearn.decomposition.PCA(
     n_components=2, random_state=42
 ).fit_transform(tsne_embeddings)
-citations["y"] = tsne_embeddings[:, 0]
-citations["x"] = tsne_embeddings[:, 1]
+citations["y"] = oriented_tsne_embeddings[:, 0]
+citations["x"] = oriented_tsne_embeddings[:, 1]
 
+# Add a score which will be used to weight the point in teh plot
 citations["score"] = [numpy.log(n + 1) for n in citations["num_citations"]]
-
 citations.sort_values("score", ascending=True, inplace=True)
 
-# Visualize
-
+# Create a formatted title for each of hte publications
 citations["TitleFormatted"] = [
     textwrap.fill(pub["title"], 40).replace("\n", "<br>")
     for pub in citations["bib_dict"]
 ]
 
-N = 10
-padding = 1.2
-
-top = citations.tail(N)
-
+# Plot and create tooltips for each publication
 fig = plotly.express.scatter(
     citations, x="x", y="y", size="score", custom_data=["TitleFormatted"]
 )
-
 fig.update_traces(
     hovertemplate="%{customdata[0]}",
     marker=dict(color="#73C1D4", opacity=1),
 )
 
+# Now let's just look at the top N publications
+top = citations.tail(N)
+
+# Let's get a right group and a left group to help with plotting
 top.sort_values("x", inplace=True)
 left = top.head(int(N / 2)).sort_values("y", ascending=False)
 right = top.tail(int(N / 2)).sort_values("y", ascending=False)
 
+# Figure out the upper and lower extents of the data
+upper_bound = numpy.max(citations["y"].values)
+lower_bound = numpy.min(citations["y"].values)
 
-fig.update_xaxes(visible=False)
-fig.update_yaxes(visible=False)
-
-
-top_extent = numpy.max(citations["y"].values)
-bottom_extent = numpy.min(citations["y"].values)
-
+# Add annotations to the plot
 for extent, data, alignment in [
     (numpy.min(citations["x"].values), left, "right"),
     (numpy.max(citations["x"].values), right, "left"),
@@ -72,7 +75,7 @@ for extent, data, alignment in [
         formatted_citation_with_link = (
             textwrap.fill(formatted_citation, 40).replace("\n", "<br>")
             + '<br><a style="color:white" href="'
-            # + data["pub_url"].values[i]
+            + str(data["pub_url"].values[i])
             + '">>>> Read Paper </a>'
         )
 
@@ -95,8 +98,8 @@ for extent, data, alignment in [
             ax=padding * extent,
             axref="x",
             y=data["y"].values[i],
-            ay=(top_extent - bottom_extent) * (int(N / 2) - i - 0.5) / int(N / 2)
-            + bottom_extent,
+            ay=(upper_bound - lower_bound) * (int(N / 2) - i - 0.5) / int(N / 2)
+            + lower_bound,
             ayref="y",
             arrowcolor="#73C1D4",
             xanchor=alignment,
@@ -104,7 +107,11 @@ for extent, data, alignment in [
             font=dict(size=10, color="#73C1D4"),
         )
 
+# Make things pretty - remove axes
+fig.update_xaxes(visible=False)
+fig.update_yaxes(visible=False)
 
+# Make things pretty - set a good margin and good colors
 fig.update_layout(
     margin=dict(l=0, r=0, t=0, b=0),
     plot_bgcolor="#191C1F",
@@ -113,6 +120,7 @@ fig.update_layout(
     ),
 )
 
+# Make things pretty - remove the plotly logo
 fig.show(
     config={
         "displaylogo": False,
