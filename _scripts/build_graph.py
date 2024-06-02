43e1d54b31d1577195943b24830423ef0e9a91f2
Chris McComb <ccmcc2012@gmail.com>
2024-06-02 14:18:24 -0400

    Updating for black

--- _scripts/build_graph.py
+++ _scripts/build_graph.py
@@ -9,34 +9,43 @@ import bs4
 
 citations = datasets.load_dataset("ccm/publications")["train"].to_pandas()
 
-tsne_embeddings = sklearn.manifold.TSNE(n_components=2, random_state=42).fit_transform(numpy.stack(citations["embedding"].values))
-tsne_embeddings = sklearn.decomposition.PCA(n_components=2, random_state=42).fit_transform(tsne_embeddings)
-citations["y"] = tsne_embeddings[:,0]
-citations["x"] = tsne_embeddings[:,1]
+tsne_embeddings = sklearn.manifold.TSNE(n_components=2, random_state=42).fit_transform(
+    numpy.stack(citations["embedding"].values)
+)
+tsne_embeddings = sklearn.decomposition.PCA(
+    n_components=2, random_state=42
+).fit_transform(tsne_embeddings)
+citations["y"] = tsne_embeddings[:, 0]
+citations["x"] = tsne_embeddings[:, 1]
 
-citations["score"] = [numpy.log(n+1) for n in citations["num_citations"]]
+citations["score"] = [numpy.log(n + 1) for n in citations["num_citations"]]
 
 citations.sort_values("score", ascending=True, inplace=True)
 
 # Visualize
 
-citations["TitleFormatted"] = [textwrap.fill(pub["title"], 40).replace("\n", "<br>") for pub in citations["bib_dict"]]
+citations["TitleFormatted"] = [
+    textwrap.fill(pub["title"], 40).replace("\n", "<br>")
+    for pub in citations["bib_dict"]
+]
 
 N = 10
 padding = 1.2
 
 top = citations.tail(N)
 
-fig = plotly.express.scatter(citations, x="x", y="y", size="score", custom_data=["TitleFormatted"])
+fig = plotly.express.scatter(
+    citations, x="x", y="y", size="score", custom_data=["TitleFormatted"]
+)
 
 fig.update_traces(
     hovertemplate="%{customdata[0]}",
     marker=dict(color="#73C1D4", opacity=1),
-    )
+)
 
 top.sort_values("x", inplace=True)
-left = top.head(int(N/2)).sort_values("y", ascending=False)
-right = top.tail(int(N/2)).sort_values("y", ascending=False)
+left = top.head(int(N / 2)).sort_values("y", ascending=False)
+right = top.tail(int(N / 2)).sort_values("y", ascending=False)
 
 
 fig.update_xaxes(visible=False)
@@ -46,43 +55,54 @@ fig.update_yaxes(visible=False)
 top_extent = numpy.max(citations["y"].values)
 bottom_extent = numpy.min(citations["y"].values)
 
-for (extent, data, alignment) in [(numpy.min(citations["x"].values), left, "right"), (numpy.max(citations["x"].values), right, "left")]:
-    for i in range(int(N/2)):
+for extent, data, alignment in [
+    (numpy.min(citations["x"].values), left, "right"),
+    (numpy.max(citations["x"].values), right, "left"),
+]:
+    for i in range(int(N / 2)):
 
-        formatted_citation = data["bib_dict"].values[i]["author"].split(" and ")[0].split(" ")[-1] + " et al. (" + str(int(data["bib_dict"].values[i]["pub_year"])) + ") \"" +data["bib_dict"].values[i]["title"] + ".\""
-        formatted_citation_with_link = textwrap.fill(formatted_citation, 40).replace("\n", "<br>") + "<br><a style=\"color:white\" href=\"" + data["pub_url"].values[i] + "\">>>> Read Paper </a>"    
-
-
-        fig.add_annotation(
-            x = data["x"].values[i],
-            ax = extent,
-            axref = "x",
-            y = data["y"].values[i],
-            ay = data["y"].values[i],
-            ayref = "y",
-            xanchor = alignment,
-            yanchor = 'middle',
-            arrowcolor = "#73C1D4",
+        formatted_citation = (
+            data["bib_dict"].values[i]["author"].split(" and ")[0].split(" ")[-1]
+            + " et al. ("
+            + str(int(data["bib_dict"].values[i]["pub_year"]))
+            + ') "'
+            + data["bib_dict"].values[i]["title"]
+            + '."'
+        )
+        formatted_citation_with_link = (
+            textwrap.fill(formatted_citation, 40).replace("\n", "<br>")
+            + '<br><a style="color:white" href="'
+            # + data["pub_url"].values[i]
+            + '">>>> Read Paper </a>'
         )
 
         fig.add_annotation(
-            align = alignment,
-            text = formatted_citation_with_link ,
-            x = extent,
-            ax = padding*extent,
-            axref = "x",
-            y = data["y"].values[i],
-            ay = (top_extent-bottom_extent)*(int(N/2) - i - 0.5)/int(N/2) + bottom_extent,
-            ayref = "y",
-            arrowcolor = "#73C1D4",
-            xanchor = alignment,
-            yanchor = 'middle',
-            font = dict(
-                size = 10,
-                color = "#73C1D4"
-            ),
+            x=data["x"].values[i],
+            ax=extent,
+            axref="x",
+            y=data["y"].values[i],
+            ay=data["y"].values[i],
+            ayref="y",
+            xanchor=alignment,
+            yanchor="middle",
+            arrowcolor="#73C1D4",
         )
 
+        fig.add_annotation(
+            align=alignment,
+            text=formatted_citation_with_link,
+            x=extent,
+            ax=padding * extent,
+            axref="x",
+            y=data["y"].values[i],
+            ay=(top_extent - bottom_extent) * (int(N / 2) - i - 0.5) / int(N / 2)
+            + bottom_extent,
+            ayref="y",
+            arrowcolor="#73C1D4",
+            xanchor=alignment,
+            yanchor="middle",
+            font=dict(size=10, color="#73C1D4"),
+        )
 
 
 fig.update_layout(
@@ -90,12 +110,13 @@ fig.update_layout(
     plot_bgcolor="#191C1F",
     hoverlabel=dict(
         bgcolor="white",
-    )
+    ),
 )
 
-fig.show(config={
-    'displaylogo': False,
-    # 'modeBarButtonsToRemove': ['select', 'lasso2d']
+fig.show(
+    config={
+        "displaylogo": False,
+        # 'modeBarButtonsToRemove': ['select', 'lasso2d']
     }
 )
 
@@ -110,4 +131,6 @@ html = open("pubs.html")
 
 soup = BeautifulSoup(html)
 
-open("_includes/graph.html", "w").write("".join([str(x) for x in soup.html.body.div.contents]))
+open("_includes/graph.html", "w").write(
+    "".join([str(x) for x in soup.html.body.div.contents])
+)
