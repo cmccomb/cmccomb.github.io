ee15623823ac5c2446737c531c4eb9d9847046c3
Chris McComb <ccmcc2012@gmail.com>
2025-06-15 15:28:24 -0400

    Lets roll!

--- _scripts/build_graph.py
+++ _scripts/build_graph.py
@@ -14,6 +14,12 @@ padding = 1.2  # Padding to give around teh annotation
 # Load data from huggingface
 citations = datasets.load_dataset("ccm/publications")["train"].to_pandas()
 
+# Add a column for the year as a number, based on the bib_dict
+citations["pub_year"] = [
+    int(pub["pub_year"]) if pub["pub_year"] is not None else 2025
+    for pub in citations["bib_dict"]
+]
+
 # Make a t-SNE embedding of the embeddings
 tsne_embeddings = sklearn.manifold.TSNE(n_components=2, random_state=42).fit_transform(
     numpy.stack(citations["embedding"].values)
@@ -27,7 +33,7 @@ citations["x"] = oriented_tsne_embeddings[:, 0]
 citations["y"] = oriented_tsne_embeddings[:, 1]
 
 # Add a score which will be used to weight the point in teh plot
-citations["score"] = [numpy.log(n + 1)+1 for n in citations["num_citations"]]
+citations["score"] = [numpy.log(n + 1) + 1 for n in citations["num_citations"]]
 citations.sort_values("score", ascending=True, inplace=True)
 
 # Create a formatted title for each of hte publications
@@ -36,17 +42,19 @@ citations["TitleFormatted"] = [
     for pub in citations["bib_dict"]
 ]
 
-
 # Plot and create tooltips for each publication
 fig = plotly.express.scatter(
-    citations, x="x", y="y", size="score", custom_data=["TitleFormatted"]
-)
-fig.update_traces(
-    hovertemplate="%{customdata[0]}",
-    marker=dict(color="#73C1D4", opacity=1),
+    citations,
+    x="x",
+    y="y",
+    size="score",
+    color="pub_year",
+    custom_data=["TitleFormatted"],
+    color_continuous_scale=plotly.colors.sequential.Agsunset,
 )
 
-citations.sort_values("score", ascending=False, inplace=True)
+fig.update_traces(hovertemplate="%{customdata[0]}")
+fig.update_coloraxes(showscale=False)
 
 centroid = citations[["x", "y"]].mean()
 
@@ -57,7 +65,7 @@ for i in range(citations.shape[0]):
     extended_citation = (
         citations["bib_dict"].values[i]["author"].split(" and ")[0].split(" ")[-1]
         + " et al. ("
-        + str(int(citations["bib_dict"].values[i]["pub_year"] or 2024))
+        + str(int(citations["bib_dict"].values[i]["pub_year"] or 2025))
         + ') "'
         + citations["bib_dict"].values[i]["title"]
         + '."'
@@ -66,15 +74,10 @@ for i in range(citations.shape[0]):
     # Create full citation with a link
     formatted_citation_with_link = (
         textwrap.fill(extended_citation, 40).replace("\n", "<br>")
-        + '<br><a style="color:white" href="'
+        + '<br><a style="color: PaleVioletRed; text-decoration: underline;" href="'
         + "https://scholar.google.com/citations?view_op=view_citation&citation_for_view="
         + citations["author_pub_id"].values[i]
-        + '">>>> Read Paper </a>'
-    )
-
-    # angle from centroid is
-    angle = numpy.arctan2(
-        citations["y"].values[i] - centroid[1], citations["x"].values[i] - centroid[0]
+        + '">Read Paper 🔗</a>'
     )
 
     # Displace notation location in direction away from centroid
@@ -82,30 +85,29 @@ for i in range(citations.shape[0]):
     y_displacement = 0
 
     # Set x and y anchor alignment based on angle
-    x_anchor = "left"
+    x_anchor = "right"
     y_anchor = "middle"
 
     # Add final annotation with label
     fig.add_annotation(
         text=formatted_citation_with_link,
-        arrowcolor="#73C1D4",
-        font=dict(size=10, color="#73C1D4"),
+        arrowcolor="#444444",
+        font=dict(size=12, color="#444444"),
         clicktoshow="onoff",
         align="left",
         xanchor=x_anchor,
         yanchor=y_anchor,
-        bgcolor="#191C1F",
-        bordercolor="#73C1D4",
+        bgcolor="#FFFFFF",
+        bordercolor="#444444",
         x=citations["x"].values[i],
         y=citations["y"].values[i],
-        ax=citations["x"].values[i] + x_displacement,
+        ax=citations["x"].values[i] - x_displacement,
         ay=citations["y"].values[i] + y_displacement,
         yclick=citations["y"].values[i],
         xclick=citations["x"].values[i],
         axref="x",
         ayref="y",
-        visible=True if i < 10 else False,
-        # Standoff as far as markersize requires adaptively
+        visible=False,
         standoff=15,
     )
 
