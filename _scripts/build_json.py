4cf986eb4187d191eed0f63dfeea43d54710f0fa
Chris McComb <ccmcc2012@gmail.com>
2025-07-12 19:40:40 -0400

    Let's see if this works

---
+++ _scripts/build_json.py
@@ -0,0 +1,42 @@
+import json
+
+import datasets
+import numpy
+import sklearn.decomposition
+import sklearn.manifold
+
+# Load data from huggingface
+citations = datasets.load_dataset("ccm/publications")["train"].to_pandas()
+
+# Add a column for the year as a number, based on the bib_dict
+citations["pub_year"] = [
+    int(pub["pub_year"]) if pub["pub_year"] is not None else 2025
+    for pub in citations["bib_dict"]
+]
+
+# Make a t-SNE embedding of the embeddings
+tsne_embeddings = sklearn.manifold.TSNE(n_components=2, random_state=42).fit_transform(
+    numpy.stack(citations["embedding"].values)
+)
+
+# Next do a PCA in order to figure out how best to orient the plot
+oriented_tsne_embeddings = sklearn.decomposition.PCA(
+    n_components=2, random_state=42
+).fit_transform(tsne_embeddings)
+citations["x"] = oriented_tsne_embeddings[:, 0]
+citations["y"] = oriented_tsne_embeddings[:, 1]
+
+# after you’ve built `citations` with x, y, score, TitleFormatted, author_pub_id, pub_year …
+payload = citations[
+    [
+        "x",
+        "y",
+        "author_pub_id",
+        "pub_year",
+        "num_citations",
+        "bib_dict",
+    ]
+].to_dict(orient="records")
+
+with open("../assets/json/pubs.json", "w") as f:
+    json.dump(payload, f, indent=2)
--- assets/js/interface.js
+++ assets/js/interface.js
@@ -1,13 +1,13 @@
 // button stuff
 document.getElementById("exit").onclick = function () {
-    if (document.documentElement.clientWidth > 768) {
+    // if (document.documentElement.clientWidth > 768) {
         document.getElementById("profile").remove();
         document.getElementById("footer").remove();
         document.getElementById("graph-container").classList.remove("blur");
         document.getElementById("graph-container").style.pointerEvents = "auto";
-    } else {
-        window.open("https://ccm-chat-with-publications.hf.space/", "_self");
-    }
+    // } else {
+    //     window.open("https://ccm-chat-with-publications.hf.space/", "_self");
+    // }
 }
 
 
