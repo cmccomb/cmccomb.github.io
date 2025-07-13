5ae1d57d5ab18ac3e3a7a54c66c31038f39b0f37
Chris McComb <ccmcc2012@gmail.com>
2025-07-13 15:07:25 -0400

    Better responsiveness

--- assets/js/interface.js
+++ assets/js/interface.js
@@ -1,13 +1,13 @@
 // button stuff
 document.getElementById("exit").onclick = function () {
-    // if (document.documentElement.clientWidth > 768) {
+    if (window.matchMedia("(max-width: 768px)").matches) {
+        window.open("https://scholar.google.com/citations?user=0P9w_S0AAAAJ&hl=en", "_self");
+    } else {
         document.getElementById("profile").remove();
         document.getElementById("footer").remove();
         document.getElementById("graph-container").classList.remove("blur");
         document.getElementById("graph-container").style.pointerEvents = "auto";
-    // } else {
-    //     window.open("https://ccm-chat-with-publications.hf.space/", "_self");
-    // }
+    }
 }
 
 
